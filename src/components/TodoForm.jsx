29c68fd91f5732600c6e71b77ebf81677fe323ec
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 16:05:03 -0500

    add handle submit and handle change

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,20 +1,40 @@
 import React, {useState} from 'react';
 
-function TodoForm() {
+function TodoForm(props) {
 
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('');
+
+    // type text into input
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    // prevent page from refreshing after clicking btn
+    const handleSubmit = e => {
+      e.preventDefault();
+
+    // generates random number within 10000 or less rage - id 
+    //  props.onSubmit({
+    //      id: Math.floor(Math.random() * 10000),
+    //      text: input
+    //  });
+
+    // makes input invisible after clicking btn  
+      setInput('')
+    };
 
     return (
         <div>
-            <form className="todo-form">
+            <form className="todo-form" onSubmit={handleSubmit}>
                 <input 
                   type="text" 
                   placeholder="Add a todo" 
                   value={input} 
                   name="text" 
                   className="todo-input"
+                  onChange={handleChange}
                 />
-            <button className='todo-btn'></button>
+            <button className='todo-btn'>button</button>
             </form>
         </div>
     )
