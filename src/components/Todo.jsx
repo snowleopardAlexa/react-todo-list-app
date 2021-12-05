9cd8b431fe3cf644290aada1d0332094a9a4330e
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 16:43:46 -0500

    add complete todo function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import TodoForm from './TodoForm';
+//import TodoForm from './TodoForm';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 
-function Todo() {
+function Todo({ todos, completeTodo }) {
 
     const [edit, setEdit] = useState({
         id: null,
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
 
@@ -16,10 +17,24 @@ function TodoList() {
         console.log(...todos);
     }
 
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatedTodos);
+    }
+
     return (
         <div className="todo-list">
             <h1>Alexa, what are you going to do today?</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo 
+              todos={todos} 
+              completeTodo={completeTodo} 
+            />
         </div>
     )
 }
