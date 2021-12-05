18b098190e8cd6f39fce9182fe2630feb2a0e9d4
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 15:55:53 -0500

    add todoform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import TodoForm from "./components/TodoForm";
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="todo-app">
+      <TodoForm />
     </div>
   );
 }
